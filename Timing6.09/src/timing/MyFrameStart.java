600e3a77dd0f11b421838398f14f5792dbfb9ec1
Zuzanna Malinska <z.malinska@gmail.com>
2015-08-04 23:09:07 +0200

    jDialog dispose po zapisie

--- Timing6.09/src/timing/MyFrameStart.java
+++ Timing6.09/src/timing/MyFrameStart.java
@@ -29,16 +29,21 @@ import javax.swing.event.ListSelectionListener;
 import com.itextpdf.text.DocumentException;
 
 public class MyFrameStart extends JFrame {
+	
+	static int wybranyIndexNaLiscie;
 
 	static Zadanie wybraneZadanie = null;
 	public static String wybranaData = Zadanie.dzisiejszaData;
+	
+	public static String pierwotnyOpis;
+	public static String opisPoZmianie;
 
 	// zegar
 	ClockLabel clock = new ClockLabel();
 
 	// dodawanie zadania z palca
 	JDialog d1;
-	final static JButton bOpenDialog = new JButton("Dodaj zadanie z palca");
+	final static JButton bWyswietlOknoDodawaniaZPalca = new JButton("Dodaj zadanie z palca");
 
 	// przejscie do edycji zadania
 	JDialog dEdycja;
@@ -161,7 +166,7 @@ public class MyFrameStart extends JFrame {
 			}
 		});
 
-		bOpenDialog.addActionListener(new ActionListener() {
+		bWyswietlOknoDodawaniaZPalca.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent zdarz) {
 
 				createAndShowGUI();
@@ -174,7 +179,7 @@ public class MyFrameStart extends JFrame {
 		bWyswietlMetryczke.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent zdarz) {
 
-				createAndShowEditionFrame();
+				createAndShowMetryczkaDialog();
 
 
 /*				EventQueue.invokeLater(new Runnable() {
@@ -351,6 +356,7 @@ public class MyFrameStart extends JFrame {
 						bWyswietlMetryczke.setEnabled(true);
 						MyFrameZadanie.ogladaneWlasnieZadanie = wybraneZadanie;
 
+						wybranyIndexNaLiscie = lZadaniaLocal.getSelectedIndex();
 						int wybranyIndex = lZadaniaLocal.getSelectedIndex();
 						lZadaniaLocal.ensureIndexIsVisible(wybranyIndex);
 
@@ -365,40 +371,124 @@ public class MyFrameStart extends JFrame {
 		});
 	}
 
-	private void createAndShowEditionFrame() {
+	private void createAndShowMetryczkaDialog() {
 
 		JDialog.setDefaultLookAndFeelDecorated(true);
-
 		dEdycja = new JDialog(this, "Metryczka zadania", true);
 
 		dEdycja.setSize(600, 600);
 		dEdycja.setLocation(350, 150);
 		dEdycja.setLayout(new FlowLayout());
+		
+		
+		final JLabel lDataZadania = new JLabel("Data zadania");
+		final JTextField fDataZadania = new JTextField(8);
+		
+		final JLabel lIdZadania = new JLabel("ID zadania");
+		final JTextField fIdZadania = new JTextField(8);
 
+		final JLabel lOd = new JLabel("Od");
+		final JTextField fCzasRozpoczecia = new JTextField(8);
+		
+		final JLabel lDo = new JLabel("Do");
+		final JTextField fCzasZakonczenia = new JTextField(8);
+		
 		final JLabel lOpis = new JLabel("Opis");
 		final JTextField fOpis = new JTextField(45);
+		
+		
+		final JLabel lCzasTrwaniaS1Faktyczny = new JLabel("Czas trwania z licznika (s1CzasTrwaniaFaktycznyS) - jeśli n.d., zadanie importowane");
+		final JTextField fCzasTrwaniaS1Faktyczny = new JTextField(40);
+		
+		
+		final JLabel lCzasOdDoS1Total = new JLabel("Czas s1CzasTotalS (czas pauz + czas z licznika)");
+		final JTextField fCzasOdDoS1Total = new JTextField(50);
+		
+		final JLabel lIlePauz = new JLabel("Ile pauz");
+		final JTextField fIlePauzZadania = new JTextField(5);
+		
+		
+		final JLabel lLacznyCzasPauz = new JLabel("Łączny czas pauz (s1CzasPauzS) - jeśli n.d., zadanie importowane");
+		final JTextField fLacznyCzasPauz = new JTextField(40);
+
+		final JLabel lCzasTrwanias3CzasTotalS = new JLabel("Czas trwania ze sparsowanych dat (s3CzasTotalS)");
+		final JTextField fCzasTrwanias3CzasTotalS = new JTextField(50);
+		
+		
 
 		final JButton bEdytujOpis = new JButton("Edytuj");
 		final JButton bZapisz = new JButton("Zapisz");
 		final JButton bPowrot= new JButton("Powrot");
 
-		//final String oldOpis = "";
+		//String oldOpis = "";
+		final String oldOpis = fOpis.getText();
 
-
+		fDataZadania.setText(wybraneZadanie.dataZadania);
+		fIdZadania.setText(String.valueOf(wybraneZadanie.iDZadania));
+		fCzasRozpoczecia.setText(wybraneZadanie.czasRozpoczeciaS);
+		fCzasZakonczenia.setText(wybraneZadanie.czasZakonczeniaS);
 		fOpis.setText(wybraneZadanie.taskName);
 		
-
+		fCzasTrwaniaS1Faktyczny.setText(wybraneZadanie.s1CzasTrwaniaFaktycznyS + " | " + wybraneZadanie.s1CzasTrwaniaFaktyczny);
+		fCzasOdDoS1Total.setText(wybraneZadanie.s1CzasTotalS + " | " + wybraneZadanie.s1CzasTotal);
+		fIlePauzZadania.setText(Integer.toString(wybraneZadanie.ilePauzZadania) ); //SOLVED 2
+		fLacznyCzasPauz.setText(wybraneZadanie.s1CzasPauzS + " | " + wybraneZadanie.s1CzasPauz);
+		fCzasTrwanias3CzasTotalS.setText(wybraneZadanie.s3CzasTotalS + " | " + wybraneZadanie.s3CzasTotal);
+		
+		//oldOpis = fOpis.getText();
+		
+		dEdycja.add(lDataZadania);
+		dEdycja.add(fDataZadania);
+		
+		dEdycja.add(lIdZadania);
+		dEdycja.add(fIdZadania);
+		
+		dEdycja.add(lOd);
+		dEdycja.add(fCzasRozpoczecia);
+		
+		dEdycja.add(lDo);
+		dEdycja.add(fCzasZakonczenia);
+		
 		dEdycja.add(lOpis);
 		dEdycja.add(fOpis);
+		
+		dEdycja.add(lCzasTrwaniaS1Faktyczny);
+		dEdycja.add(fCzasTrwaniaS1Faktyczny);
+		
+		dEdycja.add(lLacznyCzasPauz);
+		dEdycja.add(fLacznyCzasPauz);
+		
+		dEdycja.add(lIlePauz);
+		dEdycja.add(fIlePauzZadania);
+		
+		dEdycja.add(lCzasOdDoS1Total);
+		dEdycja.add(fCzasOdDoS1Total);
+		
+		dEdycja.add(lCzasTrwanias3CzasTotalS);
+		dEdycja.add(fCzasTrwanias3CzasTotalS);
 
 		dEdycja.add(bPowrot);
 		dEdycja.add(bEdytujOpis);
 		dEdycja.add(bZapisz);
 
+		fDataZadania.setEditable(false);
+		fIdZadania.setEditable(false);
+		fCzasRozpoczecia.setEditable(false);
+		fCzasZakonczenia.setEditable(false);
+		
 		fOpis.setEditable(false);
 		bZapisz.setEnabled(false);
 		bEdytujOpis.setEnabled(true);
 		
+		fCzasTrwaniaS1Faktyczny.setEditable(false);
+		fCzasOdDoS1Total.setEditable(false);
+		fIlePauzZadania.setEditable(false);
+		fLacznyCzasPauz.setEditable(false);
+		fCzasTrwanias3CzasTotalS.setEditable(false);
+		
+
+		
+		String poprzedniOpis = "";
 		
 		bPowrot.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent zdarz) {
@@ -410,39 +500,54 @@ public class MyFrameStart extends JFrame {
 
 		bEdytujOpis.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent zdarz) {
+		
 
 				fOpis.setEditable(true);
 				bZapisz.setEnabled(true);
 				bEdytujOpis.setEnabled(false);
+				
+				pierwotnyOpis = fOpis.getText();
+			
 
 			}
 		});
 
 		bZapisz.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent zdarz) {
-				String oldOpis = fOpis.getText();
+				pierwotnyOpis = wybraneZadanie.taskName;
+				System.out.println("Opis stary: "+ pierwotnyOpis);
+				
+				//String staryStary = wybraneZadanie.taskName;
+				opisPoZmianie = fOpis.getText();
+				
 				int ile = 0;
 				System.out.println(Zadanie.opisy);
 				for (Zadanie z : Zadanie.wszystkieZadania) {
-					if (z.taskName.equalsIgnoreCase(oldOpis)) {
+					if (z.taskName.equalsIgnoreCase(pierwotnyOpis)) {
 						ile++;
 					}
 				}
 				if (ile < 1) {
-					Zadanie.opisy.remove(oldOpis);
+					Zadanie.opisy.remove(pierwotnyOpis);
 				}
-				System.out.println(Zadanie.opisy);
-				wybraneZadanie.zmienOpis(fOpis.getText());
+				//System.out.println(Zadanie.opisy);
+				
+				
 				
 				if (wybraneZadanie == Zadanie.aktualneZadanie) {
-					MyFrameStart.fOpisZadania.setText(fOpis.getText());
+					MyFrameStart.fOpisZadania.setText(opisPoZmianie);
+					//fOpis.setText(opisPoZmianie);
 				}
 				
+				System.out.println("Opis po zmianie: "+ opisPoZmianie);
+				
+				
+				wybraneZadanie.zmienOpis(fOpis.getText());
 				if (!Zadanie.opisy.contains(wybraneZadanie.taskName)) {
 					Zadanie.opisy.add(wybraneZadanie.taskName);
 				}
 
-				System.out.println(Zadanie.opisy);
+				//System.out.println(Zadanie.opisy);
 				wybranaData = wybraneZadanie.dataZadania;			
 				System.out.println("Wybrana data to " + wybranaData);
 
@@ -458,6 +563,12 @@ public class MyFrameStart extends JFrame {
 				fOpis.setEditable(false);
 				bZapisz.setEnabled(false);
 				bEdytujOpis.setEnabled(true);
+				pierwotnyOpis = opisPoZmianie;
+				
+				dEdycja.dispose();
+				MyFrameStart.lZadaniaLocal.setSelectedIndex(wybranyIndexNaLiscie);;  
+				MyFrameStart.lZadaniaLocal.ensureIndexIsVisible(wybranyIndexNaLiscie);
+				
 
 			}
 
@@ -551,7 +662,7 @@ public class MyFrameStart extends JFrame {
 		add(bSaveToTXT);
 		add(tdateField);
 		add(bCalendar);
-		add(bOpenDialog);
+		add(bWyswietlOknoDodawaniaZPalca);
 
 	}
 }
